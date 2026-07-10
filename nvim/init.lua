7516cdab5b7ea80affca48ef465931a03be3167b
Andrew Niles <andrewgniles@gmail.com>
2026-07-09 21:18:09 -0600

    add git hunk navigation

--- nvim/init.lua
+++ nvim/init.lua
@@ -249,6 +249,29 @@ do
 			topdelete = { text = "‾" },
 			changedelete = { text = "~" },
 		},
+		on_attach = function(bufnr)
+			local gs = package.loaded.gitsigns
+
+			vim.keymap.set({ "n", "v" }, "]c", function()
+				if vim.wo.diff then
+					return "]c"
+				end
+				vim.schedule(function()
+					gs.next_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+
+			vim.keymap.set({ "n", "v" }, "[c", function()
+				if vim.wo.diff then
+					return "[c"
+				end
+				vim.schedule(function()
+					gs.previous_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
+		end,
 	})
 
 	-- Show pending keybinds
